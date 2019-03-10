569dc544254d6d9b9b843aa64c1ea5fe8b37cbe1
k.hamada <k.hamada.1988@gmail.com>
2019-03-10 17:04:23 +0900

    add progress text

--- src/fxtabs.tsx
+++ src/fxtabs.tsx
@@ -13,19 +13,31 @@ const realpath = util.promisify(fs.realpath)
 async function main(pathname: string) {
   try {
     const inputPath = await realpath(pathname)
+
+    console.log(`read: ${inputPath}`)
     const compressed = await readFile(inputPath)
+
+    console.log(`decompress`)
     const content = mozlz4a.decompress(compressed)
+
+    console.log(`parse json`)
     const json = JSON.parse(content.toString('UTF-8'))
     const tabs = json.windows[0].tabs
+
+    console.log(`parse tree`)
     const tabContainer = new TabContainer(tabs)
+
+    console.log(`output`)
     const list = ReactDOMServer.renderToString(List(tabContainer))
     const lastUpdate = json.session.lastUpdate || Date.now();
     const outputPath = `./fxtabs_${lastUpdate}.html`
+
+    console.log(`write: ${outputPath}`)
     await writeFile(outputPath, Template(list))
-    console.log(`success: ${outputPath}`)
+
+    console.log(`success`)
   } catch (err) {
     console.error(err)
-    return
   }
 }
 
