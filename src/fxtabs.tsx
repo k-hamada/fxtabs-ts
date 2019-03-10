5fb6399b56f82b81c065977107352dc75a21dd73
k.hamada <k.hamada.1988@gmail.com>
2019-03-10 16:23:28 +0900

    add mozlz4a, fixed #2

--- src/fxtabs.tsx
+++ src/fxtabs.tsx
@@ -2,6 +2,7 @@ import ReactDOMServer from 'react-dom/server'
 import { TabContainer } from './modules/tab_container'
 import List from './components/list'
 import Template from './components/template'
+import mozlz4a from 'mozlz4a'
 
 import fs from 'fs'
 import util from 'util'
@@ -9,11 +10,13 @@ const readFile = util.promisify(fs.readFile)
 const writeFile = util.promisify(fs.writeFile)
 
 async function main() {
-  const inputPath = './recovery.json'
+  const inputPath = './recovery.jsonlz4'
   const outputPath = `./fxtabs_${Date.now()}.html`
   try {
-    const json = await readFile(inputPath, 'utf-8')
-    const tabs = JSON.parse(json).windows[0].tabs
+    const compressed = await readFile(inputPath)
+    const content = mozlz4a.decompress(compressed)
+    const json = JSON.parse(content.toString('UTF-8'))
+    const tabs = json.windows[0].tabs
     const tabContainer = new TabContainer(tabs)
     const list = ReactDOMServer.renderToString(List(tabContainer))
     await writeFile(outputPath, Template(list))
